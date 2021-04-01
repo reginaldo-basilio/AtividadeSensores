3877e77c9b0e3fd205907163c6bdf2c1d44b4188
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2021-04-01 16:35:17 -0300

    Gerando lista de sensores

--- app/src/main/java/com/example/atividadesensores/MainActivity.java
+++ app/src/main/java/com/example/atividadesensores/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.atividadesensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    SensorManager sensorManager;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewSensor);
+
+        sensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
+        List<Sensor> sensores = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        ArrayList<String> listaSensores = new ArrayList<String>();
+        for(Sensor s : sensores){
+            listaSensores.add(s.getName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                listaSensores
+        );
+        listView.setAdapter(adapter);
     }
 }
